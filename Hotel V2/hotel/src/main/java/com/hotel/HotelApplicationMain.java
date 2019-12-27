eacd654f893f1d0cea44df260626834ebd4d478d
Nastiatv <57249641+Nastiatv@users.noreply.github.com>
2019-12-27 18:28:09 +0300

    Update HotelApplicationMain.java

--- Hotel V2/hotel/src/main/java/com/hotel/HotelApplicationMain.java
+++ Hotel V2/hotel/src/main/java/com/hotel/HotelApplicationMain.java
@@ -1,13 +1,7 @@
 package com.hotel;
 
-import java.time.LocalDate;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.hotel.api.service.IAdministrator;
-import com.hotel.enums.Status;
-import com.hotel.service.Administrator;
 
 @SpringBootApplication
 public class HotelApplicationMain {
